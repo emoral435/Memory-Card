84049685167024b788c2f40d2b3ce50622827452
Eduardo Morales <emoral435@gmail.com>
2023-02-18 13:01:57 -0600

    Base files before I start writing modules and logic

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,13 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+          <img src="public/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Not actually Vite lol</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
