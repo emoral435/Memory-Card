5523a4ae7a31e9963cf541baaade999bcbd23bda
Eduardo Morales <emoral435@gmail.com>
2023-02-18 13:06:03 -0600

    Vite Logo Fix

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="public/vite.svg" className="logo" alt="Vite logo" />
+          <img src="./assets/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
