38adb37413201cca21507f307df5dc1adb89c5d1
Eduardo Morales <emoral435@gmail.com>
2025-05-01 20:39:15 -0500

    fix: fixed some NPM issues, and fixed PNG issues on build
    Signed-off-by: Eduardo Morales <emoral435@gmail.com>

--- src/functions/returnPokeMonIndex.ts
+++ src/functions/returnPokeMonIndex.ts
@@ -1,17 +1,43 @@
+import SquirtlePNG from "../../pokemon/squirtle.png";
+import RaikouPNG from "../../pokemon/raikou.png";
+import MudkipPNG from "../../pokemon/mudkip.png";
+import LugiaPNG from "../../pokemon/lugia.png";
+import LucarioPNG from "../../pokemon/lucario.png";
+import LittenPNG from "../../pokemon/litten.png";
+import LeafyeonPNG from "../../pokemon/leafyeon.png";
+import HaxorousPNG from "../../pokemon/haxorus.png";
+import GyradosPNG from "../../pokemon/gyrados.png";
+import GengarPNG from "../../pokemon/gengar.png";
+import CharizardPNG from "../../pokemon/charizard.png";
+import ArcaninePNG from "../../pokemon/arcanine.png";
+
 const makeMon = (name: string, url: string, count: number = 0) => {
     return {
         name,
         url,
         count
-    }
-}
+    };
+};
 
 interface Pokemon {
     name: string,
     url: string,
     count: number
-}
+};
 
-const PokeDex: Pokemon[] = [makeMon("Squirtle", "/pokemon/squirtle.png",0), makeMon("Raikou", "/pokemon/raikou.png",0), makeMon("Mudkip", "/pokemon/mudkip.png",0), makeMon("Lugia", "/pokemon/lugia.png",0), makeMon("Lucario","/pokemon/lucario.png",0), makeMon("Litten", "/pokemon/litten.png",0),makeMon("Leafyeon", "/pokemon/leafyeon.png",0), makeMon("Haxorus","/pokemon/haxorus.png",0), makeMon("Gyrados","/pokemon/gyrados.png",0), makeMon("Gengar","/pokemon/gengar.png",0), makeMon("Charizard","/pokemon/charizard.png", 0), makeMon("Arcanine","/pokemon/arcanine.png", 0)]
+const PokeDex: Pokemon[] = [
+    makeMon("Squirtle", SquirtlePNG, 0), 
+    makeMon("Mudkip", MudkipPNG,0), 
+    makeMon("Raikou", RaikouPNG, 0), 
+    makeMon("Lugia", LugiaPNG, 0),
+    makeMon("Lucario", LucarioPNG, 0),
+    makeMon("Litten", LittenPNG, 0),
+    makeMon("Leafyeon", LeafyeonPNG,0),
+    makeMon("Haxorus", HaxorousPNG, 0),
+    makeMon("Gyrados", GyradosPNG, 0),
+    makeMon("Gengar", GengarPNG, 0),
+    makeMon("Charizard", CharizardPNG, 0), 
+    makeMon("Arcanine", ArcaninePNG, 0)
+];
 
-export default PokeDex
+export default PokeDex;
